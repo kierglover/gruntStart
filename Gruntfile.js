1caefb8abc352a69e83deefd728aa1649961190c
Kieran Glover <admin@Kierans-MacBook-Pro.local>
2019-05-23 15:53:23 +0700

    :tada: First commit

---
+++ Gruntfile.js
@@ -0,0 +1,60 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+
+//SIMPLE PHP SERVER //
+serve: {
+    options: {
+        port: 8000,
+    }
+},
+     // COMBINES JS //
+     concat: {
+          js: {
+               src: ['js/**/*.js'],
+               dest: 'assets/js/scripts.js',
+               },
+
+          // COMBINES CSS FILES //
+          css: {
+               src: ['css/**/*.css'],
+               dest: 'assets/css/main.css',
+               },
+     },
+
+     //UGLIFY JS//
+     uglify: {
+          my_target: {
+               files: {
+                    'build/js/scripts.min.js': ['assets/js/**/*.js']
+               }
+          }
+     },
+
+        // SENDS MAIN CSS TO BUILD CSS AND MINIFIES, REMOVES UNUSED CLASSES ETC //
+        cssnano: {
+            dist: {
+                files: {
+                    'build/css/main.min.css': 'assets/css/**/*.css'
+                }
+            }
+
+        },
+
+        watch: {
+            js: {
+                files: ['js/**/*.js', 'css/**/*.css'],
+                tasks: ['concat:js', 'concat:css', 'uglify', 'cssnano'],
+            },
+        },
+
+});
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-cssnano');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-serve');
+
+    grunt.registerTask('default', ['watch']);
+};
